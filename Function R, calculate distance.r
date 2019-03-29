758c6210eaa97be0a04a4faceb26652cc243b88d
Jorge Stephano Soriano Urbán <green.yd@hotmail.com>
2019-03-29 10:17:24 -0600

    Update Function R, calculate distance.r

--- Function R, calculate distance.r
+++ Function R, calculate distance.r
@@ -1,4 +1,3 @@
-#  La siguiente función la tomé del libro Data Science with Microsoft SQL Server 2016, 
 #  página número 30. Debido a que la función escrita en dicho libro
 #  está en lenguaje T-SQL la tranformé a lenguaje R, 
 #  además tranformé la unidad de medida de longitud del radio de la tierra de millas terrestres a kilómetros
@@ -23,9 +22,10 @@ CalculateDist <- function(long1, lat1, long2, lat2) {
     lat2 <- lat2 / 57.2958
     #  Calculando la distancia 
     distance <- (sin(lat1) * sin(lat2)) + (cos(lat1) * cos(lat2) * cos(long2 - long1))
-    #  Convirtiendo a kilómetros
+    
     if (distance != 0) {
-        distance = 6371 * atan(sqrt(1 - (distance^2)) / distance)
+        radius <- (3958.75 * 1.609344) #  Convirtiendo radio de la tierra de millas terrestres a kilómetros 
+        distance = radius * atan(sqrt(1 - (distance^2)) / distance)
         distance <- round(distance, digits = 2)
     }
     return(distance)  
